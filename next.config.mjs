d87a59cdecfaeba40657ee6c12cb022ab1318495
PPekKunGzDev <54957742+PPekKunGz@users.noreply.github.com>
2024-03-09 11:31:16 +0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
     distDir: 'out',
     reactStrictMode: true,
-    swcMinify: true,
+    swcMinify: false,
     images: {
         unoptimized: true,
         minimumCacheTTL: 60,
