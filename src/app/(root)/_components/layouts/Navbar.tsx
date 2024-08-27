81ccb5448a9ae6cc67c76ba94fd49b83e983028a
PPEKKUNGZDEV\ppekk <kittipat25er@gmail.com>
2024-08-27 19:38:51 +0700

    update

--- src/app/(root)/_components/layouts/Navbar.tsx
+++ src/app/(root)/_components/layouts/Navbar.tsx
@@ -1,50 +1,47 @@
-import { ThemeToggle } from "@/components/theme-toggle";
 import Image from "next/image";
 import Link from "next/link"
+import Head from "../../../../public/head.svg";
 
-export default function Navbar() {
-    return (
-        <header className="text-white text-center">
-            <nav className="fixed w-full h-16 shadow-xl backdrop-blur-md bg-black/30 z-50">
+export default function header() {
+  return (
+      <header className="text-white text-center">
+        <nav className="fixed w-full h-16 shadow-xl backdrop-blur-md bg-black/30 z-50">
 
-                <div className="mx-auto max-w-7xl flex items-center justify-between px-3 h-full">
-                    <div className="flex text-center items-center text-xl ml-2">
-                        <Link href="/">
-                            <Image
-                                src="favicon.ico"
-                                alt="favicon.ico"
-                                width={50}
-                                height={50}
-                                className="cursor-pointer rounded-md"
-                            />
-                        </Link>
-                        <div className="flex flex-row gap-2">
-                            <Link className="ml-5 cursor-pointer underline-animate relative font-bold" href="/">
-                                PPekKunGzDev
-                            </Link>
-                            <Link className="ml-5 cursor-pointer underline-animate relative" href="/">
-                                หน้าหลัก
-                            </Link>
-                            <div className="hidden sm:flex gap-2">
-                                <Link className="cursor-pointer underline-animate relative" href="/about">
-                                    แนะนำตัว
-                                </Link>
-                                <Link className="cursor-pointer underline-animate relative" href="/contents">
-                                    ผลงาน
-                                </Link>
-                                <Link className="cursor-pointer underline-animate relative" href="/skills">
-                                    สกิล/ภาษา
-                                </Link>
-                            </div>
-                        </div>
-                    </div>
-                    <Link className="lg:flex md:hidden text-xl cursor-pointer underline-animate relative" href="/contact">
-                        สถานะการใช้งาน
-                    </Link>
-                    {/* <ThemeToggle/> */}
+        <div className="mx-auto max-w-7xl flex items-center justify-between px-3 h-full">
+            <div className="flex text-center items-center text-xl ml-2">
+              <Link href="/">
+                <Image
+                  src="favicon.ico"
+                  alt="favicon.ico"
+                  width={50}
+                  height={50}
+                  className="cursor-pointer rounded-md"
+                />
+              </Link>
+              <div className="flex flex-row gap-2">
+                <Link className="ml-5 cursor-pointer underline-animate relative" href="/">
+                  หน้าหลัก
+                </Link>
+                <div className="hidden sm:flex gap-2">
+                  <Link className="cursor-pointer underline-animate relative" href="/about"> 
+                  แนะนำตัว
+                  </Link>
+                  <Link className="cursor-pointer underline-animate relative" href="/contents">
+                  ผลงาน
+                  </Link>
+                  <Link className="cursor-pointer underline-animate relative" href="/skills">
+                  สกิล/ภาษา
+                  </Link>
                 </div>
-
-            </nav>
-        </header>
-    )
+                
+              </div>
+            </div>
+            <Link className="text-xl cursor-pointer underline-animate relative" href="/contact">
+              สถานะการใช้งาน
+            </Link>
+          </div>
+          
+        </nav>
+    </header>
+  )
 }
--- src/app/(root)/contents/page.tsx
+++ src/app/(root)/contents/page.tsx
@@ -1,57 +1,123 @@
-"use client"
-import React, { useEffect, useState } from 'react';
-import ArticleCard from '../../../components/articlecard';
+import Link from "next/link";
 import ppekkungz from "../../../config/ppekkungz.json";
-import Dimension from '../_components/pages/Dimension';
+import Image from "next/image";
+import Dimension from "../_components/pages/Dimension";
 
-interface ArticleCardProps {
-    name: string;
-    date: string;
-    desc: string;
-    image: string;
-    lang1: string;
-    lang2: string;
-    author: string;
-    link: string;
+interface dataInterface {
+    name: string
+    img: string
+    link: string
+    data: string
+    date: string
 }
 
-export default function Blog() {
+export default function Work() {
     const data = ppekkungz;
-    const [isLoading, setIsLoading] = useState(true);
-
-    setTimeout(() => {
-        setIsLoading(false);
-    }, 2000);
     return (
-        <>
-            {isLoading ? (
-                <div className="flex items-center justify-center h-screen">
-                    <div className="animate-spin rounded-full h-12 w-12 border-4 border-primary border-t-transparent" />
+        <div className="flex flex-col pt-24 pb-10" data-aos="fade-up" data-aos-duration="2000">
+            <div className="flex justify-center pt-4 h-full w-full">
+                <div className="flex flex-wrap gap-4 px-5 justify-center">
+                    {
+                        data.map((data, index) => {
+                            return (
+                                // <div key={index} className="bg-white/10 border border-white/10 p-5 rounded-2xl w-fit h-fit">
+                                <div key={index} className="bg-gradient-to-b from-black/10 to-white/10 border-[1.8px] border-white/30 p-5 backdrop-blur-sm rounded-2xl w-fit h-fit">
+                                    <div className="flex justify-center">
+                                        <Image
+                                            src={data.image}
+                                            alt=""
+                                            width={250}
+                                            height={250}
+                                            draggable="false"
+                                            className="overflow-clip scroll-area bg-cover bg-center h-[230px] rounded-lg"
+                                        />
+                                    </div>
+                                    <div className="flex flex-col justify-start gap-1 py-1 mt-2">
+                                        <p className="text-white text-xl font-medium">{data.name}</p>
+                                        <p className="text-sm text-muted-foreground font-medium">{data.desc}</p>
+                                        {/*<p className="text-white text-xl font-medium">{data.name}</p>
+                                    <p className="text-white text-xl font-medium">{data.name}</p> */}
+                                    </div>
+                                    <div className="flex flex-col gap-2 mt-4 w-fit">
+                                        <Link href={data.link} target="_blank" className="w-fit text-sm bg-black/20 py-1 px-5 rounded-lg text-white hover:text-white/80 transition-all duration-300 cursor-pointer">รายละเอียด</Link>
+                                        <div className="w-fit text-sm bg-black/20 py-1 px-2 rounded-lg text-white hover:text-white/80 transition-all duration-300 cursor-pointer">{data.lang1}</div>
+                                        <div className="flex">
+                                            <div className="text-sm bg-black/20 py-1 px-2 rounded-lg text-white hover:text-white/80 transition-all duration-300 cursor-pointer">{data.lang2}</div>
+                                        </div>
+                                    </div>
+                                </div>
+                            )
+                        })
+                    }
                 </div>
-            ) : (
-                <div className="min-h-screen h-full container mx-auto pt-24 p-6">
-                    <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-                        {data.map((data: any, index: number) => (
-                            <div key={index}>
-                                <ArticleCard
-                                    image={data.image}
-                                    name={data.name}
-                                    author={data.author}
-                                    date="Date 00, 2020"
-                                    desc={data.desc}
-                                    link={data.link}
-                                    lang1={data.lang1}
-                                    lang2={data.lang2}
-                                />
-                            </div>
-                        ))}
-                    </div>
-                    <div className="mt-5" />
-                    <div id="Dimension">
-                        <Dimension />
-                    </div>
-                </div>
-            )}
-        </>
-    );
-};
+            </div>
+            <div className="mt-5" />
+            <div id="Dimension">
+                <Dimension />
+            </div>
+        </div>
+    )
+}
+
+
+
+
+
+
+// "use client"
+// import React, { useEffect, useState } from 'react';
+// import ArticleCard from '../../../components/articlecard';
+// import ppekkungz from "../../../config/ppekkungz.json";
+// import Dimension from '../_components/pages/Dimension';
+
+// interface ArticleCardProps {
+//     name: string;
+//     date: string;
+//     desc: string;
+//     image: string;
+//     lang1: string;
+//     lang2: string;
+//     author: string;
+//     link: string;
+// }
+
+// export default function Blog() {
+//     const data = ppekkungz;
+//     const [isLoading, setIsLoading] = useState(true);
+
+//     setTimeout(() => {
+//         setIsLoading(false);
+//     }, 2000);
+//     return (
+//         <>
+//             {isLoading ? (
+//                 <div className="flex items-center justify-center h-screen">
+//                     <div className="animate-spin rounded-full h-12 w-12 border-4 border-primary border-t-transparent" />
+//                 </div>
+//             ) : (
+//                 <div className="min-h-screen h-full container mx-auto pt-24 p-6">
+//                     <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+//                         {data.map((data: any, index: number) => (
+//                             <div key={index}>
+//                                 <ArticleCard
+//                                     image={data.image}
+//                                     name={data.name}
+//                                     author={data.author}
+//                                     date="Date 00, 2020"
+//                                     desc={data.desc}
+//                                     link={data.link}
+//                                     lang1={data.lang1}
+//                                     lang2={data.lang2}
+//                                 />
+//                             </div>
+//                         ))}
+//                     </div>
+//                     <div className="mt-5" />
+//                     <div id="Dimension">
+//                         <Dimension />
+//                     </div>
+//                 </div>
+//             )}
+//         </>
+//     );
+// };
